7e66664ca90dfa5eea9b45d41649291408e83923
Wong Shouhao <wongshouhao@outlook.com>
2023-04-07 23:11:13 +0800

    Delete TxRouter interface in mockERC20

--- script/MockERC20.s.sol
+++ script/MockERC20.s.sol
@@ -6,18 +6,6 @@ import "forge-std/Script.sol";
 
 import "../test/mock/mockERC20.sol";
 
-interface TxRouter {
-    struct PermitCall {
-        uint256 ownerValueV;
-        bytes32 r;
-        bytes32 s; 
-    }
-
-    function multiTransfer(address, uint256[] memory) external;
-    function multiApproveTransfer(address, address, uint256[] memory) external;
-    function multiAggregate(address, address, uint256, PermitCall[] memory) external;
-}
-
 contract Deploy is Script {
   function run() external  {
     vm.startBroadcast();
