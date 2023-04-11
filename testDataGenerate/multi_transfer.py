140a590b1ba6e02897c22548a12c15eed5d635d7
Wong Shouhao <wongshouhao@outlook.com>
2023-04-11 10:49:57 +0800

    Fix multi_transfer

--- testDataGenerate/multi_transfer.py
+++ testDataGenerate/multi_transfer.py
@@ -7,7 +7,7 @@ def main():
 	recevier_num = eval(input("Please input recevier count: "))
 
 	for _ in range(recevier_num):
-		receiver_address = input("Please input recevier address(prefix 0x): ")
+		receiver_address = input("Please input recevier address(not with prefix 0x): ")
 		amount = eval(input("Please input transfer amount: ")) * 10 ** token_decimals
 		hex_amount = hex(amount)[2:].zfill(24)
 		multi_transfer.append(receiver_address + hex_amount)
@@ -15,7 +15,8 @@ def main():
 	result = "["
 	for i in multi_transfer:
 		result += i
-	result += "]"
+		result += ","
+	result += "\b]"
 
 	print(result)
 
